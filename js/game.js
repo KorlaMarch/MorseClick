e261a82096279effed039584ea0abb92b6b540c2
Khemarat Boonyapaluk <korla.march@gmail.com>
2015-05-11 21:39:24 +0700

    Remove moblie support

--- js/game.js
+++ js/game.js
@@ -1,7 +1,4 @@
 $( document ).ready(function(){
-	$( document ).on("dbltap", function(event) {
-		event.gesture.preventDefault();
-	});
 	new GameManager();
 });
 
@@ -9,7 +6,6 @@ function GameManager(){
 	var self = this;
 	$(document).keydown(function(event){ if(event.which!=13) self.down(); });
 	$('.button').mousedown(function(event){ self.down(); });
-	$('.button').on("tap",function(event){ self.down(); });
 	$(document).keyup(function(event){ 
 		if(event.which==13){
 			self.pause = !self.pause;
